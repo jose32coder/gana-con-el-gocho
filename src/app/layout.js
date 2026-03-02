0772a7cfc472537e2ee86e1b220fe65ca4637aaa
jose32coder <josemanuel180601@gmail.com>
2026-03-02 15:23:51 -0400

    Script de FBPixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,6 +38,27 @@ export default function RootLayout({ children }) {
           rel="dns-prefetch"
           href="https://idwyfhufvgalylnvzyqr.supabase.co"
         />
+        // src/app/layout.js
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '1253074576243445');
+    fbq('track', 'PageView');
+
+    // Estos son los eventos con los parámetros ya listos
+    fbq('track', 'ViewContent');
+    fbq('track', 'Contact');
+    fbq('track', 'Purchase', {value: 0.00, currency: 'USD'});
+  `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
