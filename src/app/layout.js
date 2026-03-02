0066df043d49844592dd2cc7b14bc45dfc61a170
jose32coder <josemanuel180601@gmail.com>
2026-03-02 15:26:09 -0400

    Script de FBPixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,6 +26,8 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const pixelId = process.env.NEXT_PUBLIC_FB_PIXEL_ID;
+
   return (
     <html lang="en">
       <head>
@@ -38,27 +41,23 @@ export default function RootLayout({ children }) {
           rel="dns-prefetch"
           href="https://idwyfhufvgalylnvzyqr.supabase.co"
         />
-        // src/app/layout.js
-        <Script id="fb-pixel" strategy="afterInteractive">
-          {`
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
 
-    fbq('init', '1253074576243445');
-    fbq('track', 'PageView');
-
-    // Estos son los eventos con los parámetros ya listos
-    fbq('track', 'ViewContent');
-    fbq('track', 'Contact');
-    fbq('track', 'Purchase', {value: 0.00, currency: 'USD'});
-  `}
-        </Script>
+        {pixelId && (
+          <Script id="fb-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${pixelId}');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+        )}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
